3e9444c6923260f676480870c91709cbf65a4896
Marcos Freiberger <marcos.freiberger@bradesco.com.br>
2023-01-25 22:27:05 -0300

    Configurando o React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About/About';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
