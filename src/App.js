fb6b69738da29e005305fce68464bdafbe606ddb
Marcos Freiberger <marcos.freiberger@bradesco.com.br>
2023-01-26 16:38:55 -0300

    Criando componentes de navbar e footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import About from './pages/About/About';
 import Home from './pages/Home/Home';
 
@@ -7,12 +9,14 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className='container'>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
