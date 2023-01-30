b3dcd467070f262d32b0cf4ee778fd0f48cda453
Marcos Freiberger <marcos.freiberger@bradesco.com.br>
2023-01-30 16:46:03 -0300

    Links para usuário autenticado

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import { useAuthentication } from "./hooks/useAuthentication";
 import { useEffect, useState } from "react";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -37,6 +39,8 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/posts/create" element={<CreatePost />} />
+              <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
           </div>
           <Footer />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,75 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import styles from './Navbar.module.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useAuthValue } from "../context/AuthContext";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-    return (
-        <nav className={styles.navbar}>
-            <NavLink to='/' className={styles.brand}>
-                Mini <span>Blog</span>
-            </NavLink>
-            <ul className={styles.links_list}>
-                <li>
-                    <NavLink to='/' className={({ isActive }) => (isActive ? styles.active : '')}>Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/login' className={({ isActive }) => (isActive ? styles.active : '')}>Entrar</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/register' className={({ isActive }) => (isActive ? styles.active : '')}>Cadastrar</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/about' className={({ isActive }) => (isActive ? styles.active : '')}>Sobre</NavLink>
-                </li>
-            </ul>
-        </nav>
-    )
-}
+  const { user } = useAuthValue();
+  return (
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={styles.brand}>
+        Mini <span>Blog</span>
+      </NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        {user && (
+          <>
+            <li>
+              <NavLink
+                to="/posts/create"
+                className={({ isActive }) => (isActive ? styles.active : "")}
+              >
+                Novo post
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dashboard"
+                className={({ isActive }) => (isActive ? styles.active : "")}
+              >
+                Dashboard
+              </NavLink>
+            </li>
+          </>
+        )}
+        {!user && (
+          <>
+            <li>
+              <NavLink
+                to="/login"
+                className={({ isActive }) => (isActive ? styles.active : "")}
+              >
+                Entrar
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/register"
+                className={({ isActive }) => (isActive ? styles.active : "")}
+              >
+                Cadastrar
+              </NavLink>
+            </li>
+          </>
+        )}
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Sobre
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
