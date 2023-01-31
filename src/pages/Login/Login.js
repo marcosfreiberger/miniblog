4d5313dfc6ba7ccb9869d5c4cf30b7281d8ca21f
Marcos Freiberger <marcos.freiberger@bradesco.com.br>
2023-01-31 14:29:28 -0300

    Estruturando página de login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,12 +1,79 @@
-import React from 'react'
-import styles from './Login.module.css'
+import React, { useEffect, useState } from "react";
+import { useAuthentication } from "../../hooks/useAuthentication";
+import styles from "./Login.module.css";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>Login</h2>
-        </div>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-export default Login
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const clearForm = () => {
+    setEmail("");
+    setPassword("");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = {
+      email,
+      password,
+    };
+
+    const res = await createUser(user);
+
+    console.log(res);
+    clearForm();
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
+  return (
+    <div className={styles.login}>
+      <h1>Entrar</h1>
+
+      <p>Faça o login para poder utilizar o sistema</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="E-mail do usuário"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        {!loading && <button className="btn">Entrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
